5c9fe78a98e754d442c877660eeca81523ac71d4
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-07-06 19:23:00 +0500

    Nullable properrty

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,11 +1,11 @@
 void main(List<String> arguments) {
-  Student student1 = new Student();
-  student1.studentName = "Ali";
-  student1.fatherName = "Ahmed";
-  student1.address = "Malir Karachi";
-  student1.age = 10;
-  student1.course = "Flutter";
-  student1.dateOfAddmission = "2024";
+  Student student1 = Student("Shaheer", "Flutter");
+  // student1.studentName = "Ali";
+  // student1.fatherName = "Ahmed";
+  // student1.address = "Malir Karachi";
+  // student1.age = 10;
+  // student1.course = "Flutter";
+  // student1.dateOfAddmission = "2024";
 
   print(student1.course);
   print(student1.runtimeType);
@@ -15,7 +15,12 @@ class Student {
   String? studentName;
   String? fatherName;
   String? address;
-  int? age;
+  int age;
   String? course;
   String? dateOfAddmission;
+
+  Student(this.studentName, this.course);
+
+  // This is because when you call a class it assign Null values by default to all
+  // properties. and when you make a property not nullable it shows error.
 }
