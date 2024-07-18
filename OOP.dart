31eb028444c57479e73d03e546446ed4daa92fb0
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-07-18 15:25:54 +0500

    create file. OOP with example, advantages, RoadMap

---
+++ OOP.dart
@@ -0,0 +1,34 @@
+void main(List<String> args) {
+  /// Dart OOP
+  ///
+  /// What is OOP???
+  /// A technique by which we create objects. And the concepts used in creating the applications
+  /// by the interaction of these objects are called object oriented programming.
+  /// 
+  /// Example: Person
+  ///     Properties: Name, Age, Contact, Height, Weight.
+  ///     Behaviors: Talking, Walking, Eating, Sleeping.
+  /// 
+  /// Advantages: 
+  ///     Increase the reuseability.
+  ///     Decrease Comlexity.
+  ///     Maintaining the code.
+  ///     Easy to understand.
+  ///     Avoid the repeatation of code.
+  ///     
+  /// RoadMap:
+  ///     Classes
+  ///     Objects
+  ///     Constructors
+  ///     Fetter Setter
+  ///     Inheritence
+  ///     Polymorphysum
+  ///     Encapsulation
+  ///     Abstraction
+  ///     Abstract Classes
+  ///     Interfaces
+  ///     Maxin classes
+  ///     Factory Constructor  etc...
+  /// 
+  /// 
+}
