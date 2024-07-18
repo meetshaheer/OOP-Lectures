47504ccc9c93d5212e768d84b16a9d1909ab9cb0
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-07-18 15:37:22 +0500

    null safety

--- OOP.dart
+++ OOP.dart
@@ -4,18 +4,18 @@ void main(List<String> args) {
   /// What is OOP???
   /// A technique by which we create objects. And the concepts used in creating the applications
   /// by the interaction of these objects are called object oriented programming.
-  /// 
+  ///
   /// Example: Person
   ///     Properties: Name, Age, Contact, Height, Weight.
   ///     Behaviors: Talking, Walking, Eating, Sleeping.
-  /// 
-  /// Advantages: 
+  ///
+  /// Advantages:
   ///     Increase the reuseability.
   ///     Decrease Comlexity.
   ///     Maintaining the code.
   ///     Easy to understand.
   ///     Avoid the repeatation of code.
-  ///     
+  ///
   /// RoadMap:
   ///     Classes
   ///     Objects
@@ -29,6 +29,19 @@ void main(List<String> args) {
   ///     Interfaces
   ///     Maxin classes
   ///     Factory Constructor  etc...
-  /// 
-  /// 
+  ///
+  /// Dart Null Safety:
+
+  int age;
+  // print(age);
+  // show error while printing.
+  // non-nullable local variable 'age'.
+  // means never be null, must have value.
+
+  int? weight;
+  print(weight);
+  weight = 34;
+  print(weight);
+  // this question mark with data type indicate that this variable is nullable.
+  // 
 }
