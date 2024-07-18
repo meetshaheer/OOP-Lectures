4fc44536d20b3059141becdace9d7469e8ef302d
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-07-18 15:44:14 +0500

    Definign Classes

---
+++ Classes.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  ///
+  /// Class:
+  ///   class is a blueprint for creatign objects
+  ///   
+  /// Blue Print:
+  ///   Example: Iphone is an objects. Millions of Objects.
+  ///   First someone design a model of iphone. then it develop.
+  /// 
+  /// Class is simply a model. having properties, behaviors.
+  
+}
